65fc009f560a081566891d5b865285f15d361557
hackerman-x <kv.muhammad.shahid@gmail.com>
2025-10-19 10:21:45 +0530

    Added Gun Shooting mechanics

---
+++ Scripts/bullet.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+@export var speed := 2.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	translate(Vector3(0, 0, -speed * delta))
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody3D
 
+@export var camera3d: Camera3D
+
+var previous_x: float = 0.0
 var speed
 const WALK_SPEED = 5.0
 const SPRINT_SPEED = 8.0
@@ -14,12 +17,14 @@ const BASE_FOV = 75.0
 const FOV_CHANGE = 1.5
 
 var gravity = 9.8
+var camera_set = false
 
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
 
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	if camera3d:
+		previous_x = camera3d.global_position.x
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -27,7 +32,23 @@ func _unhandled_input(event: InputEvent) -> void:
 		camera.rotate_x(-event.relative.y * SENSETIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 
+func _process(_delta: float) -> void:
+	if not camera3d:
+		return
+
+	var current_x = camera3d.global_position.x
+	if current_x != previous_x:
+		print("Camera X changed! ΔX =", current_x - previous_x)
+		# Do something when camera moves in X
+
+	previous_x = current_x
+
 func _physics_process(delta: float) -> void:
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		camera_set = true
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -35,6 +56,9 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	if camera_set and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		$Head/Camera3D/Rifle.shoot()
 	
 	#SPRINT
 	if Input.is_action_pressed("sprint"):
